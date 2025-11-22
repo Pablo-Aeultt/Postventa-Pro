afa94dbf493db5601ec350ffde92f4131ccf8cce
Pablo <pablo.aeultt@inacapmail.cl>
2025-11-22 20:05:16 -0300

    redireccionamiento boton postventa pro corregido

--- postventa_app/templatetags/propietario_extras.py
+++ postventa_app/templatetags/propietario_extras.py
@@ -1,5 +1,5 @@
 from django import template
-from ..models import Propietario
+from ..models import Propietario, Tecnico
 
 register = template.Library()
 
@@ -20,3 +20,14 @@ def propietario_for_user(user):
             if (p.rut or '').replace('.', '').replace('-', '').lower() == username_clean.lower():
                 return p
     return None
+
+@register.filter
+def user_is_tecnico(user):
+    """Verifica si el usuario es un técnico"""
+    if not user or not getattr(user, 'is_authenticated', False):
+        return False
+    try:
+        Tecnico.objects.get(id_user=user)
+        return True
+    except Tecnico.DoesNotExist:
+        return False
